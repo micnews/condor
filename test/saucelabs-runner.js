40edd9adc78fd85c32d8846b282f6cf8ff7cf3d7
David Björklund <david.bjorklund@gmail.com>
2014-08-25 14:01:50 +0200

    set correct options in browser.init

--- test/saucelabs-runner.js
+++ test/saucelabs-runner.js
@@ -8,6 +8,7 @@ var test = require('gap')
   , browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
   , tests = require('bulk-require')(__dirname, [ '*-test.js' ])
   , tunnel
+  , build = (new Date()).toJSON()
 
 require('co')(function* () {
   var browsers = yield require('./utils/saucelabs')
@@ -15,7 +16,13 @@ require('co')(function* () {
   // TODO: run this in parallel
   browsers.forEach(function (browserConfig) {
     test('setup (' + browserConfig.name + ' ' + browserConfig.version + ')', function* () {
-      yield browser.init(browserConfig)
+      yield browser.init({
+          name: 'condor'
+        , browserName: browserConfig.name
+        , version: browserConfig.version
+        , platform: browserConfig.platform
+        , build: build
+      })
       yield server.listen.bind(server, 0)
       tunnel = yield localtunnel.bind(localtunnel, server.address().port)
       server.url = tunnel.url
