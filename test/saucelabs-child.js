942bd440e3fe4e4d772f6b013fbecef3b55fd162
David Björklund <david.bjorklund@gmail.com>
2014-08-26 16:32:01 +0200

    run each browser in a subprocess

---
+++ test/saucelabs-child.js
@@ -0,0 +1,32 @@
+var localtunnel = require('localtunnel')
+  , test = require('gap')
+  , tests = require('bulk-require')(__dirname, [ '*-test.js' ])
+  , browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
+  , utils = require('./utils')
+  , server = utils.createServer()
+  , browserConfig = JSON.parse(process.argv[2])
+  , build = process.argv[3]
+
+console.log('Browser: %s %s', browserConfig.name, browserConfig.version)
+test('setup browser, server & tunnel', function* () {
+  yield server.listen.bind(server, 0)
+  tunnel = yield localtunnel.bind(localtunnel, server.address().port)
+  server.url = tunnel.url
+  yield browser.init({
+      name: 'condor'
+    , browserName: browserConfig.name
+    , version: browserConfig.version
+    , platform: browserConfig.platform
+    , build: build
+  })
+})
+
+Object.keys(tests).forEach(function (key) {
+  tests[key](server, browser)
+})
+
+test('teardown browser & server', function* () {
+  server.unref()
+  tunnel.close()
+  yield browser.quit()
+})
--- test/saucelabs-runner.js
+++ test/saucelabs-runner.js
@@ -2,42 +2,26 @@
 
 var test = require('gap')
   , localtunnel = require('localtunnel')
+  , path = require('path')
 
   , utils = require('./utils')
-  , server = utils.createServer()
-  , browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
-  , tests = require('bulk-require')(__dirname, [ '*-test.js' ])
-  , tunnel
   , build = (new Date()).toJSON()
+  , spawn = require('child_process').spawn
 
 require('co')(function* () {
   var browsers = yield require('./utils/saucelabs')
 
-  // TODO: run this in parallel
-  browsers.forEach(function (browserConfig) {
-    test('setup (' + browserConfig.name + ' ' + browserConfig.version + ')', function* () {
-      yield browser.init({
-          name: 'condor'
-        , browserName: browserConfig.name
-        , version: browserConfig.version
-        , platform: browserConfig.platform
-        , build: build
-      })
-      yield server.listen.bind(server, 0)
-      tunnel = yield localtunnel.bind(localtunnel, server.address().port)
-      server.url = tunnel.url
-    })
-
-    Object.keys(tests).forEach(function (key) {
-      tests[key](server, browser)
-    })
-
-    test('teardown', function* () {
-      yield browser.quit()
-      tunnel.close()
-      server.close()
-    })
-  })
-
-
+  for(var i = 0; i < browsers.length; ++i) {
+    var child = spawn(
+        path.resolve(__dirname, '../node_modules/.bin/gnode')
+      , [
+            __dirname + '/saucelabs-child.js'
+          , JSON.stringify(browsers[i])
+          , build
+        ]
+    )
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+    yield child.once.bind(child, 'exit')
+  }
 })()
