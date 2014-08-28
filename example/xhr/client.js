6699c95bbfcb16b9812fe3f8c7625d8fb45eb081
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-27 18:11:48 -0700

    useable example clients

--- example/xhr/client.js
+++ example/xhr/client.js
@@ -1,39 +1,7 @@
-var debounce = require('debounce')
-  , xhr = require('xhr')
-  , condor = require('../../condor')()
+//run the xhr client, overriding the default path
+require('../../xhr')({path: '/condor'})
 
-  , noop = function () {}
-  , data = []
-
-    // save all events happening within a second and send them in one POST
-    //  request
-  , batchPost = debounce(function () {
-      var body = data.join('\n')
-      data = []
-
-      xhr({
-          method: 'POST'
-        , body: body
-        , uri: '/condor'
-        , response: true
-      }, noop)
-    }, 1000)
-
-condor.onevent = function (csv) {
-  data.push(csv)
-  batchPost()
-}
-
-// this gets called by beforeunload - so anything in here must be synchronous
-condor.onend = function (csv) {
-  data.push(csv)
-
-  // this will be an end-event - meaning that the visit on the page has ended
-  xhr({
-      method: 'POST'
-    , body: data.join('\n')
-    , uri: '/track'
-    , sync: true
-    , response: true
-  }, noop)
-}
+// if you have installed condor with npm
+// then this will look like:
+//   require('condor/xhr')({path: '/condor'})
+// instead.
