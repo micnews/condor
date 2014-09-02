0a98aa7ff08f57064a6591746f1ec94f0e4dbd1c
David Björklund <david.bjorklund@gmail.com>
2014-09-02 22:36:37 +0200

    New custom runner (to be extracted to generic module)

--- test/saucelabs-runner.js
+++ test/saucelabs-runner.js
@@ -14,56 +14,78 @@ var co = require('co')
       var server = require('./utils/server')()
       server.listen(0, function () { done(null, server) })
     }
-  , runTestsOnBrowser = function* (config) {
-      config = extend(
-          {
-              name: 'condor'
-            , build: build
-            , public: 'public'
-          }
-        , config
-      )
 
-      var browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
-        , server = yield startServer
-        , tunnel = yield localtunnel.bind(localtunnel, server.address().port)
-        , browserName = config.browserName + ' ' + config.version
-
-      server.url = tunnel.url
-
-      console.log('queued', browserName)
-      yield browser.init(config)
-      console.log('started', browserName)
-
-        var result = []
-        , harness = tape.createHarness()
-        , stream = harness.createStream()
-        , test = gap.inject(harness)
-        , buf
-
-      Object.keys(tests).forEach(function (key) {
-        tests[key](test, server, browser)
-      })
-
-      while(buf = yield read(stream)) {
-        result.push(buf)
-      }
-
-      yield browser.quit()
-      console.log('finished', browserName)
-      console.log(result.join(''))
-
-      server.unref()
-      tunnel.close()
+  , baseConfig = {
+        name: 'condor'
+      , build: build
+      , public: 'public'
     }
 
-  , local = process.argv.indexOf('--local') !== -1 || process.argv.indexOf('-l') !== -1
+  , customRunner = function* (baseConfig, browsers, setupTests) {
+
+      console.log('Start running test')
+      console.log(baseConfig)
+
+      var browserConfigs = (yield getBrowsers.bind(null, browsers)).map(function (config) {
+            return extend(baseConfig, config)
+          })
+
+      yield each(
+          browserConfigs
+        , function* (config) {
+            var browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
+              , browserName = config.browserName + ' ' + config.version
+              , result = []
+              , harness = tape.createHarness()
+              , test = gap.inject(harness)
+              , buf
+              , stream
+
+            console.log('Queueing', browserName)
+            yield browser.init(config)
+
+            console.log('Running tests on ', browserName)
+            yield setupTests(test, browser)
+
+            // harness.createStream starts running the tests setup in setupTests
+            stream = harness.createStream()
+
+            console.log('stream', stream)
+
+            while(buf = yield read(stream)) {
+              console.log('read from stream')
+              result.push(buf.toString())
+            }
+
+            yield browser.quit()
+            console.log('finished', browserName)
+            console.log(result.join(''))
+          }
+      )
+    }
 
 co(function* () {
-  var configs = yield getBrowsers.bind(null, require('../package.json').browsers)
+  yield customRunner(
+      baseConfig
+    // , require('../package.json').browsers
+    , [ 'internet explorer/10' ]
+    , function* (test, browser) {
 
-  yield each(
-      configs
-    , runTestsOnBrowser
+        var server = yield startServer
+          , tunnel = yield localtunnel.bind(localtunnel, server.address().port)
+
+        server.url = tunnel.url
+
+        Object.keys(tests).forEach(function (key) {
+          tests[key](test, server, browser)
+        })
+
+        // run this in a test-closure to have it running last
+        test('tearDown', function* (t) {
+          console.log('tearDown')
+          server.close()
+          tunnel.close()
+        })
+      }
   )
 })()
