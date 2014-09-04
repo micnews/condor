42dd0654c04381574f624272ed0df55529a863d0
David Björklund <david.bjorklund@gmail.com>
2014-09-04 14:09:43 +0200

    saucelabs-runner -> runner, beginning of a phantomjs-runner

--- test/runner.js
+++ test/runner.js
@@ -1,36 +1,135 @@
-// this file must be run in gnode or similar, to have generators support
+var execFile = require('child_process').execFile
 
-var utils = require('./utils')
-  , gap = require('gap')
-  , tape = require('tape')
-  , server = utils.createServer()
-  , read = require('co-read')
+  , chalk = require('chalk')
   , co = require('co')
-    // TODO: use phantomjs for this somehow
-  , browser = require('co-wd').remote('http://localhost:9515')
+  , each = require('co-each')
+  , extend = require('xtend')
+  , gap = require('gap')
+  , getBrowsers = require('get-saucelabs-browsers')
+  , localtunnel = require('localtunnel')
+  , read = require('co-read')
+  , tape = require('tape')
   , tests = require('bulk-require')(__dirname, [ '*-test.js' ])
+  , thunkify = require('thunkify')
+
+  , build = process.env.TRAVIS_BUILD_NUMBER || (new Date()).toJSON()
+
+  , startServer = function (done) {
+      var server = require('./utils/server')()
+      server.listen(0, function () { done(null, server) })
+    }
+
+  , saucelabsRunner = function* (baseConfig, browsers, setupTests) {
+
+      var browsers = yield thunkify(getBrowsers)(browsers)
+        , browserConfigs = browsers.map(function (config) {
+            return extend(baseConfig, config)
+          })
+
+      yield each(
+          browserConfigs
+        , function* (config) {
+            var browser = require('co-wd').remote('ondemand.saucelabs.com', 80)
+              , testOutput = []
+              , harness = tape.createHarness()
+              , test = gap.inject(harness)
+              , buf
+              , stream
+              , log = function (msg) {
+                  var string = config.browserName + ' ' + config.version + ': ' + msg
+                  console.log(chalk.magenta(string))
+                }
+
+            log('Queued')
+            yield browser.init(config)
+
+            log('Started')
+            yield setupTests(test, browser)
+
+            // harness.createStream starts running the tests setup in setupTests
+            stream = harness.createStream()
+
+            while(buf = yield read(stream)) {
+              testOutput.push(buf.toString())
+            }
+
+            // fail test on sauce labs if the test fail
+            if (harness._results.fail > 0)
+              yield browser.sauceJobStatus(false)
+
+            yield browser.quit()
+            log('Finished')
+            console.log(testOutput.join(''))
+          }
+      )
+    }
+  , localRunner = function* (setupTests) {
+      // -w means that phantomjs starts with a webdriver connector on port 8910
+      var phantomjs = execFile(require('phantomjs').path, [ '-w' ])
+        , browser = require('co-wd').remote('localhost', 8910)
+        , harness = tape.createHarness()
+        , buf
+        , stream
+
+      // try to connect to the webdriver connector until it gets live
+      yield function tryToConnect (done) {
+        var net = require('net')
+
+        var connection = net.connect(8910)
+        connection.once('connect', function () {
+          connection.end()
+          done()
+        })
+        connection.once('error', function () {
+          setTimeout(function () { tryToConnect(done) }, 100 )
+        })
+      }
+
+      yield browser.init()
+
+      yield setupTests(harness, browser)
+
+      // harness.createStream starts running the tests setup in setupTests
+      stream = harness.createStream()
+      while(buf = yield read(stream)) {
+        process.stdout.write(buf)
+      }
+      yield browser.quit()
+      phantomjs.kill()
+    }
+  , setupTests = function* (test, browser) {
+      var server = yield startServer
+        , tunnel = yield thunkify(localtunnel)(server.address().port)
+
+      server.url = tunnel.url
+
+      Object.keys(tests).forEach(function (key) {
+        tests[key](test, server, browser)
+      })
+
+      // run this in a test-closure to have it running last
+      test('tearDown', function* (t) {
+        server.close()
+        tunnel.close()
+      })
+    }
+
+  , local = process.argv.indexOf('--local') !== -1
 
 co(function* () {
-  yield server.listen.bind(server, 0)
-  yield browser.init()
-  server.url = 'http://localhost:' + server.address().port
+  if (local) {
+    yield localRunner(setupTests)
+  } else {
+    var baseConfig = {
+            name: 'condor'
+          , build: build
+          , public: 'public'
+        }
 
-    var result = []
-    , harness = tape.createHarness()
-    , stream = harness.createStream()
-    , test = gap.inject(harness)
-    , buf
-
-  Object.keys(tests).forEach(function (key) {
-    tests[key](test, server, browser)
-  })
-
-  while(buf = yield read(stream)) {
-    result.push(buf)
+    yield saucelabsRunner(
+        baseConfig
+      , require('../package.json').browsers
+      , setupTests
+    )
   }
-
-  console.log(result.join(''))
-
-  yield browser.quit()
-  yield server.close.bind(server)
 })()
